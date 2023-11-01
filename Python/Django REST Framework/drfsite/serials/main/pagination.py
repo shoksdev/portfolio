86714111feacd412053f200a911efc2d71ba8c71
shoksdev <138390299+shoksdev@users.noreply.github.com>
2023-11-01 17:53:40 +0700

    Tidy up Github

--- Python/Django REST Framework/drfsite/serials/main/pagination.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import pagination
-
-
-# Пагинация, на странице выводится два объекта из списка, но количество может увеличиваться
-class MainPagination(pagination.PageNumberPagination):
-    page_size = 2
-    page_size_query_param = 'page_size'
-    max_page_size = 4
--- Python/Django REST Framework/drfsite/serials/serials/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('api/v1/', include('main.urls')),
-    path('api/v1/api-auth/', include('rest_framework.urls')),  # Стандартная аутентификация DRF
-    path('admin/', admin.site.urls),
-]
